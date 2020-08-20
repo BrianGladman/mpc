21668770977368459c74723f2bddad38b727ea20
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2020-08-20 08:56:54 +0200

    added missing include
    Reported by Andreas, while using mpc-1.2.0 to bootstrap Guix,
    where mpc-1.2.0 is used to compile gcc-7.5.0:
    
    In file included from ../../../gcc-7.5.0/mpc/src/asin.c:22:0:
    ../../../gcc-7.5.0/mpc/src/asin.c: In function 'mpc_asin_series':
    ../../../gcc-7.5.0/mpc/src/asin.c:132:27: error: 'ULONG_MAX' undeclared (first use in this function)
       MPC_ASSERT(2 * k - 1 <= ULONG_MAX / (2 * k - 1));

--- src/asin.c
+++ src/asin.c
@@ -19,6 +19,7 @@ along with this program. If not, see http://www.gnu.org/licenses/ .
 */
 
 #include <stdio.h>
+#include <limits.h> /* for ULONG_MAX */
 #include "mpc-impl.h"
 
 /* Special case op = 1 + i*y for tiny y (see algorithms.tex).
