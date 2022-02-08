2ab55ee455acf05bd65dac07e21ff06d79685076
Andreas Enge <andreas.enge@inria.fr>
2022-02-08 14:50:32 +0100

    Rewrite AGM tests with a file and add generic tests.
    The return values are taken from the computed results and as such are only
    a regression test.
    
    * tests/agm.dat, tests/agm.dsc: New files.
    * tests/Makefile.am: Register new files.
    * tests/tagm.c (test_agm): Remove function.
      (main): Use the data file and generic test machinery.

--- tests/tagm.c
+++ tests/tagm.c
@@ -1,4 +1,4 @@
-/* targ -- test file for mpc_agm.
+/* tagm -- test file for mpc_agm.
 
 Copyright (C) 2022 INRIA
 
@@ -20,56 +20,26 @@ along with this program. If not, see http://www.gnu.org/licenses/ .
 
 #include "mpc-tests.h"
 
-static int
-test_agm (void)
-{
-   mpc_t a, b, z;
-   mpfr_prec_t prec = 1000;
-
-   mpc_init2 (a, prec);
-   mpc_init2 (b, prec);
-   mpc_init2 (z, prec);
-
-   /* Example where a is real and b is close to -a and which causes
-      cancellation in the first step. */
-   mpc_set_ui_ui (a, 1, 0, MPC_RNDNN);
-   mpc_mul_2ui (a, a, 100, MPC_RNDNN);
-   mpc_neg (b, a, MPC_RNDNN);
-   mpfr_add_ui (mpc_realref (b), mpc_realref (b), 1, MPFR_RNDZ);
-   mpfr_set_ui (mpc_imagref (b), 1, MPFR_RNDN);
-   mpc_agm (z, a, b, MPC_RNDNN);
-
-   /* Example where a is real and b is close to a and which causes a loss
-      in the error analysis when switching from a relative error to an
-      absolute error (in ulp) for the imaginary part. */
-   mpc_set (b, a, MPC_RNDNN);
-   mpfr_set_ui (mpc_imagref (b), 1, MPFR_RNDN);
-   mpc_agm (z, a, b, MPC_RNDNN);
-
-   /* Example where a is real and b is close to -a and which causes a loss
-      in the error analysis when switching from a relative error to an
-      absolute error (in ulp) for the real part. */
-   mpc_neg (b, a, MPC_RNDNN);
-   mpfr_set_ui (mpc_imagref (b), 1, MPFR_RNDN);
-   mpc_agm (z, a, b, MPC_RNDNN);
-
-   /* Non-trivial example with an angle of pi. */
-   mpc_set_ui_ui (a, 1, 2, MPC_RNDNN);
-   mpc_set_si_si (b, -10, -20, MPC_RNDNN);
-   mpc_agm (z, a, b, MPC_RNDNN);
-
-   /* Non-trivial example with an angle 0. */
-   mpc_set_ui_ui (a, 1, 2, MPC_RNDNN);
-   mpc_set_ui_ui (b, 10, 20, MPC_RNDNN);
-   mpc_agm (z, a, b, MPC_RNDNN);
-
-   return 0;
-}
+#define MPC_FUNCTION_CALL                                               \
+  P[0].mpc_inex = mpc_agm (P[1].mpc, P[2].mpc, P[3].mpc, P[4].mpc_rnd)
+#define MPC_FUNCTION_CALL_SYMMETRIC                                     \
+  P[0].mpc_inex = mpc_agm (P[1].mpc, P[3].mpc, P[2].mpc, P[4].mpc_rnd)
+#define MPC_FUNCTION_CALL_REUSE_OP1                                     \
+  P[0].mpc_inex = mpc_agm (P[1].mpc, P[1].mpc, P[3].mpc, P[4].mpc_rnd)
+#define MPC_FUNCTION_CALL_REUSE_OP2                                     \
+  P[0].mpc_inex = mpc_agm (P[1].mpc, P[2].mpc, P[1].mpc, P[4].mpc_rnd)
+#include "data_check.tpl"
+#include "tgeneric.tpl"
 
 
 int
 main (void)
 {
-  return test_agm ();
+   test_start ();
+
+   data_check_template ("agm.dsc", "agm.dat");
+   tgeneric_template ("agm.dsc", 2, 4096, 41, 1024);
+
+   return 0;
 }
 
