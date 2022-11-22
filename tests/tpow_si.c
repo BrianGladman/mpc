bfbe1671a29092acf588f8162c1c912295054273
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2022-11-22 07:53:35 +0100

    fixed wrong test in compare_mpc_pow
    We can't loop for rnd from 0 to 15, since it will lead to invalid or
    unsupported roundings.

--- tests/tpow_si.c
+++ tests/tpow_si.c
@@ -29,7 +29,7 @@ compare_mpc_pow (mpfr_prec_t pmax, int iter, unsigned long nbits)
   mpc_t x, y, z, t;
   long n;
   int i, inex_pow, inex_pow_si;
-  mpc_rnd_t rnd;
+  mpc_rnd_t rnd1, rnd2, rnd;
 
   mpc_init3 (y, sizeof (unsigned long) * CHAR_BIT, MPFR_PREC_MIN);
   for (p = MPFR_PREC_MIN; p <= pmax; p++)
@@ -41,8 +41,10 @@ compare_mpc_pow (mpfr_prec_t pmax, int iter, unsigned long nbits)
         mpc_urandom (x, rands);
         n = (signed long) gmp_urandomb_ui (rands, nbits);
         mpc_set_si (y, n, MPC_RNDNN);
-        for (rnd = 0; rnd < 16; rnd ++)
+        for (rnd1 = 0; rnd1 < 4; rnd1 ++)
+          for (rnd2 = 0; rnd2 < 4; rnd2 ++)
           {
+            rnd = MPC_RND (rnd1, rnd2);
             inex_pow = mpc_pow (z, x, y, rnd);
             inex_pow_si = mpc_pow_si (t, x, n, rnd);
             if (mpc_cmp (z, t) != 0)
