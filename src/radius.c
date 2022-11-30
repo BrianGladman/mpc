1b84b4cf5e02cbe4fabe5e47a2abd9c63640cb71
Andreas Enge <andreas.enge@inria.fr>
2022-11-30 16:40:15 +0100

    Correctly print int64_t variables.
    Reported by Mickaël Gastineau <Mickael.Gastineau@obspm.fr> and
    Bruno Haible <bruno@clisp.org>.
    
    * src/radius.c (mpcr_out_str), tests/tradius.c (test_sqrt): Use PRIi64
      as format modifier.
    * tests/mpc-tests.h, src/radius.c: Include inttypes.h.

--- src/radius.c
+++ src/radius.c
@@ -20,6 +20,7 @@ along with this program. If not, see http://www.gnu.org/licenses/ .
 
 #include <stdio.h>
 #include <math.h>
+#include <inttypes.h> /* for the PRIi64 format modifier */
 #include "mpc-impl.h"
 
 #define MPCR_MANT(r) ((r)->mant)
@@ -411,7 +412,7 @@ void mpcr_out_str (FILE *f, mpcr_srcptr r)
    else if (mpcr_zero_p (r))
       fprintf (f, "0");
    else {
-      fprintf (f, "±(%li, %li)", MPCR_MANT (r), MPCR_EXP (r));
+      fprintf (f, "±(%"PRIi64", %"PRIi64")", MPCR_MANT (r), MPCR_EXP (r));
    }
 }
 
